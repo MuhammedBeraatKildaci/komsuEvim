b762d70c5f987cb437f10f730dd6f00d86c6cce7
Muhammed Beraat Kıldacı <muhammedberaatkildaci@Muhammed-MacBook-Air.local>
2025-07-21 18:43:46 +0300

    create web front end project and router has been done

---
+++ frontend/src/App.tsx
@@ -0,0 +1,10 @@
+import { RouterProvider } from "react-router"
+import router from "./routes/router.tsx"
+
+const App = () => {
+  return (
+    <RouterProvider router={router} />
+  )
+}
+
+export default App
---
+++ frontend/src/routes/router.tsx
@@ -0,0 +1,32 @@
+import { createBrowserRouter } from "react-router"
+import Home from "../pages/Home"
+import About from "../pages/About"
+import Profile from "../pages/Profile"
+import SignIn from "../pages/SignIn"
+import SignUp from "../pages/SignUp"
+
+const router = createBrowserRouter([
+  {
+    index: true,
+    path: '/',
+    element: <Home/>,
+  },
+  {
+    path: '/about',
+    element: <About/>,
+  },
+  {
+    path: '/profile',
+    element: <Profile/>,
+  },
+  {
+    path: '/signin',
+    element: <SignIn/>,
+  },
+  {
+    path: '/signup',
+    element: <SignUp/>,
+  },
+])
+
+export default router
